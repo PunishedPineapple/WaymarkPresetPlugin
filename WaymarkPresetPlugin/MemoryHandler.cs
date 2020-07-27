27b6267105cfdcc95ed6b568921c5dc8cbc5cdd9
PunishedPineapple <50609717+PunishedPineapple@users.noreply.github.com>
2020-07-27 15:50:44 -0500

    Made memory handler a proper static class, got rid of stuff that we didn't really need.

--- WaymarkPresetPlugin/MemoryHandler.cs
+++ WaymarkPresetPlugin/MemoryHandler.cs
@@ -10,36 +10,31 @@ using Dalamud.Plugin;
 
 namespace WaymarkPresetPlugin
 {
-	public class MemoryHandler
+	public static class MemoryHandler
 	{
-		public MemoryHandler( DalamudPluginInterface pluginInterface )
+		public static void Init( DalamudPluginInterface pluginInterface )
 		{
 			//	Get Function Pointers
-			var getConfigFileAddress = pluginInterface.TargetModuleScanner.ScanText( "E8 ?? ?? ?? ?? 48 85 C0 74 14 83 7B 44 00" );
-			if( getConfigFileAddress != IntPtr.Zero )
+			IntPtr fpGetUISAVESectionAddress = pluginInterface.TargetModuleScanner.ScanText( "40 53 48 83 EC 20 48 8B 0D ?? ?? ?? ?? 0F B7 DA" );
+			if( fpGetUISAVESectionAddress != IntPtr.Zero )
 			{
-				mfpGetConfigFile = Marshal.GetDelegateForFunctionPointer<GetConfigFileDelegate>( getConfigFileAddress );
+				mdGetUISAVESectionAddress = Marshal.GetDelegateForFunctionPointer<GetConfigSectionDelegate>( fpGetUISAVESectionAddress );
 			}
-			var getConfigSectionAddress = pluginInterface.TargetModuleScanner.ScanText( "40 53 48 83 EC 20 48 8B 0D ?? ?? ?? ?? 0F B7 DA" );
-			if( getConfigSectionAddress != IntPtr.Zero )
+
+			IntPtr fpGetPresetAddressForSlot = pluginInterface.TargetModuleScanner.ScanText( "4C 8B C9 85 D2 78 0A 83 FA 08 73 05" );
+			if( fpGetPresetAddressForSlot != IntPtr.Zero )
 			{
-				mfpGetConfigSection = Marshal.GetDelegateForFunctionPointer<GetConfigSectionDelegate>( getConfigSectionAddress );
-			}
-			var getPresetAddressForSlot = pluginInterface.TargetModuleScanner.ScanText( "4C 8B C9 85 D2 78 0A 83 FA 08 73 05" );
-			if( getPresetAddressForSlot != IntPtr.Zero )
-			{
-				mfpGetPresetAddressForSlot = Marshal.GetDelegateForFunctionPointer<GetPresetAddressForSlotDelegate>( getPresetAddressForSlot );
+				mdGetPresetAddressForSlot = Marshal.GetDelegateForFunctionPointer<GetPresetAddressForSlotDelegate>( fpGetPresetAddressForSlot );
 			}
 		}
 
-		public bool FoundAllSigs()
+		public static bool FoundSavedPresetSigs()
 		{
-			return	mfpGetConfigFile != null &&
-					mfpGetConfigSection != null &&
-					mfpGetPresetAddressForSlot != null;
+			return	mdGetUISAVESectionAddress != null &&
+					mdGetPresetAddressForSlot != null;
 		}
 
-		public byte[] ReadSlot( uint slotNum )
+		public static byte[] ReadSlot( uint slotNum )
 		{
 			IntPtr pWaymarkData = GetGameWaymarkDataPointerForSlot( slotNum );
 			byte[] data = new byte[104];
@@ -52,7 +47,7 @@ namespace WaymarkPresetPlugin
 			return data;
 		}
 
-		public bool WriteSlot( uint slotNum, byte[] data )
+		public static bool WriteSlot( uint slotNum, byte[] data )
 		{
 			IntPtr pWaymarkData = GetGameWaymarkDataPointerForSlot( slotNum );
 			if( data.Length >= 104 && pWaymarkData != IntPtr.Zero )
@@ -67,36 +62,31 @@ namespace WaymarkPresetPlugin
 			}
 		}
 
-		public IntPtr GetGameWaymarkDataPointerForSlot( uint slotNum )
+		public static IntPtr GetGameWaymarkDataPointerForSlot( uint slotNum )
 		{
-			if( !FoundAllSigs() || slotNum < 1 || slotNum > 5 )
+			if( !FoundSavedPresetSigs() || slotNum < 1 || slotNum > 5 )
 			{
 				return IntPtr.Zero;
 			}
 			else
 			{
-				IntPtr pWaymarksLocation = mfpGetConfigSection.Invoke( mfpGetConfigFile.Invoke( 0x9 ), 0x11 );
+				IntPtr pWaymarksLocation = mdGetUISAVESectionAddress.Invoke( IntPtr.Zero, 0x11 );
 
 				if( pWaymarksLocation != IntPtr.Zero )
 				{
-					pWaymarksLocation = mfpGetPresetAddressForSlot( pWaymarksLocation, slotNum - 1 );
+					pWaymarksLocation = mdGetPresetAddressForSlot.Invoke( pWaymarksLocation, slotNum - 1 );
 				}
 
 				return pWaymarksLocation;
 			}
 		}
 
-		public IntPtr GetAlternateWaymarkDataPointer( uint slotNum )
-		{
-			return mfpGetPresetAddressForSlot != null ? mfpGetPresetAddressForSlot.Invoke( mfpGetConfigSection.Invoke( mfpGetConfigFile.Invoke( 0x9 ), 0x11 ), slotNum ) : IntPtr.Zero;
-		}
+		private delegate IntPtr GetConfigSectionDelegate( IntPtr pConfigFile, byte sectionIndex );
+		private delegate IntPtr GetPresetAddressForSlotDelegate( IntPtr pMarkerDataStart, uint slotNum );
+		private delegate byte GetCurrentContentLinkTypeDelegate();
+		private delegate uint GetCurrentZoneDelegate();
 
-		protected delegate IntPtr GetConfigFileDelegate( byte fileIndex );
-		protected delegate IntPtr GetConfigSectionDelegate( IntPtr pConfigFile, byte sectionIndex );
-		protected delegate IntPtr GetPresetAddressForSlotDelegate( IntPtr pMarkerDataStart, uint slotNum );
-
-		protected static GetConfigFileDelegate mfpGetConfigFile;
-		protected static GetConfigSectionDelegate mfpGetConfigSection;
-		protected static GetPresetAddressForSlotDelegate mfpGetPresetAddressForSlot;
+		private static GetConfigSectionDelegate mdGetUISAVESectionAddress;
+		private static GetPresetAddressForSlotDelegate mdGetPresetAddressForSlot;
 	}
 }
