545245853ea48403bf86d9f533cdc8992503b0c0
squidmade <squidmade@gmail.com>
2022-07-14 16:40:28 -0500

    Prevent unselect when a preset is double clicked and AllowUnselectPreset is enabled. Single click still unselects the preset.

--- WaymarkPresetPlugin/UI/Window_Library.cs
+++ WaymarkPresetPlugin/UI/Window_Library.cs
@@ -285,12 +285,12 @@ namespace WaymarkPresetPlugin
 			var indices = zonePresets.Value;
 			for( int i = 0; i < indices.Count; ++i )
 			{
-				if( ImGui.Selectable( $"{mConfiguration.PresetLibrary.Presets[indices[i]].Name}{( mConfiguration.ShowLibraryIndexInPresetInfo ? " (" + indices[i].ToString() + ")" : "" )}###_Preset_{indices[i]}", indices[i] == SelectedPreset ) )
+				if( ImGui.Selectable( $"{mConfiguration.PresetLibrary.Presets[indices[i]].Name}{( mConfiguration.ShowLibraryIndexInPresetInfo ? " (" + indices[i].ToString() + ")" : "" )}###_Preset_{indices[i]}", indices[i] == SelectedPreset, ImGuiSelectableFlags.AllowDoubleClick ) )
 				{
 					//	It's probably a bad idea to allow the selection to change when a preset's being edited.
 					if( !mUI.EditorWindow.EditingPreset )
 					{
-						if( mConfiguration.AllowUnselectPreset && indices[i] == SelectedPreset )
+						if( mConfiguration.AllowUnselectPreset && indices[i] == SelectedPreset && !ImGui.IsMouseDoubleClicked( ImGuiMouseButton.Left ) )
 						{
 							SelectedPreset = -1;
 						}
@@ -301,14 +301,14 @@ namespace WaymarkPresetPlugin
 
 						mUI.InfoPaneWindow.CancelPendingDelete();
 					}
-				}
 
-				// Place preset when its entry in the library window is double clicked.
-				if ( SelectedPreset >= 0 && ImGui.IsItemHovered() && ImGui.IsMouseDoubleClicked( ImGuiMouseButton.Left ) )
-				{
-					var preset = mConfiguration.PresetLibrary.Presets[SelectedPreset].GetAsGamePreset();
-				
-					MemoryHandler.PlacePreset( preset );
+					// Place preset when its entry in the library window is double clicked.
+					if( SelectedPreset >= 0 && ImGui.IsItemHovered() && ImGui.IsMouseDoubleClicked( ImGuiMouseButton.Left ) )
+					{
+						var preset = mConfiguration.PresetLibrary.Presets[SelectedPreset].GetAsGamePreset();
+
+						MemoryHandler.PlacePreset( preset );
+					}
 				}
 
 				if( !mUI.EditorWindow.EditingPreset && mConfiguration.AllowPresetDragAndDropOrdering && ImGui.BeginDragDropSource( ImGuiDragDropFlags.SourceNoHoldToOpenOthers ) )
